ef5c27d2f83422535e89f4b5080c58c7972a2520
KaranAJ2023 <136151737+KaranAJ2023@users.noreply.github.com>
2024-07-24 11:56:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function displayWindowSize(){
+let w =window.innerWidth;
+let h = window.innerHeight;
+
+let size=document.getElementById(‘sizeInfo’).innerHTML =("<h1>" + "Width: " + w + ", Height: " + h + "</h1>");
+}
+window.addEventListener("resize", displayWindowSize);
+displayWindowSize();
